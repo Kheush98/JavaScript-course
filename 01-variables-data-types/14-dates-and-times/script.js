dc8f7be917680fa331c52023867a9767ad5cff8d
Cheikh GAYE <gayec270198@gmail.com>
2023-10-29 11:18:33 +0000

    update

--- 01-variables-data-types/14-dates-and-times/script.js
+++ 01-variables-data-types/14-dates-and-times/script.js
@@ -7,7 +7,7 @@ d = d.toString();
 d = new Date(2021, 0, 10, 12, 0, 0);
 
 d = new Date('2021-07-10T12:00:00')
-d = new Date('07/12/202212:00:00')
+d = new Date('07/12/2022 12:00:00')
 d = new Date('2021-07-10')
 
 console.log(d);
