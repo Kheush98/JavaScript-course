e07880b266b5bdb28b4f0427e8ebcbef2c68db19
Cheikh GAYE <gayec270198@gmail.com>
2023-11-01 12:04:00 +0000

    array challenge

--- 02-arrays-and-objects/04-array-challenge/script.js
+++ 02-arrays-and-objects/04-array-challenge/script.js
@@ -1,28 +1,18 @@
-// Challenge 1
+'use strict';
 
-const arr = [1, 2, 3, 4, 5];
+//Challenge 1
+const arr = [1, 2, 3, 4,5];
 
-// arr.reverse();
-// arr.push(0);
-// arr.unshift(6);
-
-// Same result as above
-arr.push(6);
-arr.unshift(0);
-arr.reverse();
+arr.reverse().unshift(6);
+arr.push(0);
 
 console.log(arr);
 
-// Challenge 2
-
+//Challenge 2
 const arr1 = [1, 2, 3, 4, 5];
-const arr2 = [5, 6, 7, 8, 9, 10];
+const arr2 = [5,6,7,8, 9, 10];
 
-// Solution 1
-const arr3 = arr1.slice(0, 4).concat(arr2);
+arr1.pop();
+const arr3 = [...arr1, ...arr2];
 
-// Solution 2
-const arr4 = [...arr1, ...arr2];
-arr4.splice(4, 1);
-
-console.log(arr4);
+console.log(arr3);
