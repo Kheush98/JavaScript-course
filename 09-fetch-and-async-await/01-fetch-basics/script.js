6bd454cdd88eb7489324d5adfa4693fe604eacb2
Cheikh GAYE <gayec270198@gmail.com>
2023-11-15 20:52:43 +0000

    fetch basics

---
+++ 09-fetch-and-async-await/01-fetch-basics/script.js
@@ -0,0 +1,16 @@
+let arr = [];
+// Fetching a JSON file
+fetch('./movies.json')
+  .then(response =>  response.json())
+  .then(data => console.log(data));
+
+// Fetching a text file
+fetch('./test.txt')
+  .then(response =>  response.text())
+  .then(data => console.log(data));
+
+// Fetching from an API
+fetch('https://api.github.com/users/kheush98')
+  .then(response =>  response.json())
+  .then(data => (document.querySelector('h1').textContent = data.login));
+
