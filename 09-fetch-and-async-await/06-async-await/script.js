88ae575ab1927448f6a2195430fdc72c8e26fbf4
Cheikh GAYE <gayec270198@gmail.com>
2023-11-20 08:19:33 +0000

    asyc await

---
+++ 09-fetch-and-async-await/06-async-await/script.js
@@ -0,0 +1,33 @@
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve({
+      name: 'John',
+      age: 25
+    });
+  }, 1000);
+});
+
+// promise.then(data => console.log(data));
+
+async function getPromise() {
+  const response = await promise;
+  console.log(response);
+}
+
+async function getUsers() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+
+  console.log(data);
+}
+
+const getPosts = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+
+  console.log(data);
+}
+
+// getPromise();
+// getUsers();
+getPosts();
