2b9fd148a37b0af8fe487627a2a8dbf73b275734
Cheikh GAYE <gayec270198@gmail.com>
2024-01-28 10:09:06 +0000

    testing my palindrome alog

---
+++ 15-unit-testing-algorithms/04-palindrome/algo-testing/palindrome/palindrom.test.js
@@ -0,0 +1,23 @@
+const palindrome = require('./palindrome');
+
+describe('Palindrome', () => {
+  it('should be a function', () => {
+    expect(typeof palindrome).toEqual('function');
+  });
+  it('should return a function', () => {
+    expect(typeof palindrome('hello')).toEqual('boolean');
+  });
+  it('should return true if is a palindrome', () => {
+    expect(palindrome('wow')).toBeTruthy();
+    expect(palindrome('kayak')).toBeTruthy();
+    expect(palindrome('miccim')).toBeTruthy();
+  });
+  it('should return false if is not palindrome', () => {
+    expect(palindrome('home')).toBeFalsy();
+    expect(palindrome('school')).toBeFalsy();
+    expect(palindrome('monsieur')).toBeFalsy();
+  });
+  it('should return false if has spaces', () => {
+    expect(palindrome('  wow')).toBeFalsy();
+  })
+});
