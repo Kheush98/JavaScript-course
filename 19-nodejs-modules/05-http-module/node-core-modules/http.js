4dab01f93430bd2692b771484de9f202a8cf429f
Cheikh GAYE <gayec270198@gmail.com>
2024-02-03 23:48:54 +0000

    http module

--- 19-nodejs-modules/05-http-module/node-core-modules/http.js
+++ 19-nodejs-modules/05-http-module/node-core-modules/http.js
@@ -2,29 +2,26 @@ const http = require('http');
 const fs = require('fs');
 
 const posts = [
-  { id: 1, title: 'Post One', body: 'This is post 1' },
-  { id: 2, title: 'Post Two', body: 'This is post 2' },
-];
-
+  {id: 1, title: 'Post 1', body: 'This is post one'},
+  {id: 2, title: 'Post 2', body: 'This is post two'}
+]
 const server = http.createServer((request, response) => {
   const url = request.url;
 
   if (url === '/') {
-    fs.readFile('index.html', (error, file) => {
-      if (error) {
-        response.writeHead(500, { 'content-type': 'text/html' });
-        response.end('<h1>Sorry, we have a problem</h1>');
+    fs.readFile('index.html', (err, file) => {
+      if (err) {
+        response.writeHead(500, {'content-type': 'text/html'});
+        response.end('<h1>Server is down</h1>');
       } else {
-        response.writeHead(200, { 'content-type': 'text/html' });
+        response.writeHead(200, {'content-type': 'text/html'});
         response.end(file);
       }
-    });
-  } else if (url === '/api/posts') {
-    response.writeHead(200, { 'content-type': 'application/json' });
-    response.end(JSON.stringify({ success: true, data: posts }));
-  } else {
-    response.writeHead(404, { 'content-type': 'text/html' });
-    response.end('<h1>Page Not Found</h1>');
+    })
+  }
+  if (url === '/api/posts') {
+    response.writeHead(200, {'content-type': 'application/json'});
+    response.end(JSON.stringify({success: true, data: posts}));
   }
 });
 
